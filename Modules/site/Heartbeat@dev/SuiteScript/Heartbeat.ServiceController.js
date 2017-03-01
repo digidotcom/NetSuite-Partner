2f06b0f4a7af72a6a1e06fb3b3abd60d21ee1a9f
Joaquin Gatica <joaquingatica@gmail.com>
2017-03-01 17:58:23 -0300

    Add https local and heartbeat

---
+++ Modules/site/Heartbeat@dev/SuiteScript/Heartbeat.ServiceController.js
@@ -0,0 +1,27 @@
+define('Heartbeat.ServiceController', [
+    'Application',
+    'ServiceController'
+], function HeartbeatServiceController(
+    Application,
+    ServiceController
+) {
+    'use strict';
+
+    return ServiceController.extend({
+        name: 'Heartbeat.ServiceController',
+
+        get: function () {
+            var beatsParam = this.request.getParameter('beat');
+            var beats = null;
+
+            if (beatsParam) {
+                beats = parseInt(beatsParam, 10);
+            }
+
+            return {
+                acknowledged: true,
+                beats: beats
+            };
+        }
+    });
+});
