f748bb84f9ba6becaa3c7ceff16570032d01856c
Joaquin Gatica <joaquingatica@gmail.com>
2017-03-28 16:13:12 -0300

    Load statuses dynamically

--- Modules/site/Registration@dev/SuiteScript/Registration.Status.Model.js
+++ Modules/site/Registration@dev/SuiteScript/Registration.Status.Model.js
@@ -1,42 +1,53 @@
 define('Registration.Status.Model', [
     'underscore',
-    'SC.Model'
+    'SC.Model',
+    'SearchHelper'
 ], function RegistratioStatusModel(
     _,
-    SCModel
+    SCModel,
+    SearchHelper
 ) {
     'use strict';
+
+    function booleanMap(line, v) {
+        return line.getValue(v.fieldName) === 'T';
+    }
 
     return SCModel.extend({
         name: 'Registration.Status',
 
+        record: 'customrecord_registration_status',
+        columns: {
+            internalid: { fieldName: 'internalid' },
+            name: { fieldName: 'custrecord_registration_status_public' },
+            allowsEdit: { fieldName: 'custrecord_registration_status_edit', applyFunction: booleanMap }
+        },
+        filters: [
+            { fieldName: 'isinactive', operator: 'is', value1: 'F' }
+        ],
+        sort: { fieldName: 'internalid', order: 'asc' },
+        fieldsets: {
+            list: [
+                'internalid',
+                'name',
+                'allowsEdit'
+            ]
+        },
+
         list: function list() {
-            return [
-                {
-                    internalid: 1,
-                    name: 'Approved'
-                },
-                {
-                    internalid: 2,
-                    name: 'Expired'
-                },
-                {
-                    internalid: 3,
-                    name: 'Open'
-                },
-                {
-                    internalid: 4,
-                    name: 'Not Submitted'
-                },
-                {
-                    internalid: 5,
-                    name: 'Pending'
-                },
-                {
-                    internalid: 6,
-                    name: 'Rejected'
-                }
-            ];
+            var search = new SearchHelper()
+                .setRecord(this.record)
+                .setFilters(this.filters)
+                .setColumns(this.columns)
+                .setFieldset(this.fieldsets.list);
+
+            if (this.sort) {
+                search
+                    .setSort(this.sort.fieldName)
+                    .setSortOrder(this.sort.order);
+            }
+
+            return search.search().getResults();
         }
     });
 });
