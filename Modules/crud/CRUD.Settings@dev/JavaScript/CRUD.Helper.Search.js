2c488dc5d7fde1adf2ddca91cfa339a25092b9ef
Joaquin Gatica <joaquingatica@gmail.com>
2017-10-30 18:22:37 -0300

    CRUD search results page

--- Modules/crud/CRUD.Settings@dev/JavaScript/CRUD.Helper.Search.js
+++ Modules/crud/CRUD.Settings@dev/JavaScript/CRUD.Helper.Search.js
@@ -10,11 +10,17 @@ define('CRUD.Helper.Search', [
     'use strict';
 
     return {
+        getSearchResultsUrl: function getSearchResultsUrl() {
+            return 'crud/search';
+        },
+        getSearchQueryKey: function getSearchQueryKey() {
+            return 'q';
+        },
         getSearchConfig: function getSearchConfig(crudId) {
             return CrudConfiguration.get(crudId).search || {};
         },
         isSearchable: function isSearchable(crudId) {
-            return !!this.getSearchConfig(crudId);
+            return _(this.getSearchConfig(crudId)).size() > 0;
         },
         getSearchableIds: function getSearchableIds() {
             var self = this;
@@ -23,11 +29,16 @@ define('CRUD.Helper.Search', [
                 return self.isSearchable(crudId);
             });
         },
-        getSearchServiceUrl: function getSearchServiceUrl(crudIds, absolute) {
+        getSearchServiceUrl: function getSearchServiceUrl(crudIds, query, absolute) {
             var crudIdSingle = (crudIds && crudIds[0]) || '';
             var url = CrudConfiguration.getStatic(crudIdSingle).searchServiceUrl;
+            var queryKey = this.getSearchQueryKey();
             url += (url.indexOf('?') >= 0) ? '&' : '?';
             url += 'ids=' + crudIds.join(',');
+            if (query) {
+                url += '&';
+                url += queryKey + '=' + query;
+            }
             return absolute ? Utils.getAbsoluteUrl(url) : url;
         }
     };
--- Modules/crud/CRUD@dev/JavaScript/CRUD.Search.Collection.js
+++ Modules/crud/CRUD@dev/JavaScript/CRUD.Search.Collection.js
@@ -11,17 +11,27 @@ define('CRUD.Search.Collection', [
 ) {
     'use strict';
 
-    return Backbone.CachedCollection.extend({
+    return Backbone.Collection.extend({
 
         model: CrudSearchModel,
 
         initialize: function initialize(elements, options) {
             this.crudIds = (options && options.crudIds && options.crudIds.length > 0) || CrudHelper.getSearchableIds();
+            this.query = (options && options.query) || null;
             this.setUrl();
         },
 
         setUrl: function setUrl() {
-            this.url = CrudHelper.getSearchServiceUrl(this.crudIds, true);
+            this.url = CrudHelper.getSearchServiceUrl(this.crudIds, this.query, true);
+        },
+
+        update: function update(options) {
+            if (this.query) {
+                this.fetch({
+                    reset: true,
+                    killerId: options.killerId
+                });
+            }
         }
 
     });
